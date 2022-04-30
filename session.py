cd6e97301c11a2d77faa94cddc3cc9f55d7d1fbe
João Fonsaka <joaovitorbf@protonmail.com>
2022-04-30 20:16:56 -0300

    Improve time precision

--- session.py
+++ session.py
@@ -3,14 +3,14 @@ import urllib
 from PIL import ImageTk, Image
 from io import BytesIO
 from threading import Timer, main_thread
-from time import sleep
+from time import sleep, time
 
 class Session:
     def __init__(self, ui):
         self.req = requests.session()
         self.ui = ui
         self.length = 1
-        self.position = 0
+        self.initialtime = time()
 
         self.update_interface()
 
@@ -31,13 +31,14 @@ class Session:
 
         self.length = info['song']['length']
         self.position = info['song']['position']
+        self.initialtime = int(time())-info['song']['position']
         self.update_time()
 
     def update_time(self):
         if not main_thread().is_alive(): return
-        self.ui.set_time(self.length, self.position)
+        self.ui.set_time(self.length, int(time())-self.initialtime)
+        print(int(time())-self.initialtime)
 
-        if self.position < self.length:
-            self.position += 1
-            Timer(1.0, self.update_time).start()
+        if int(time())-self.initialtime < self.length:
+            Timer(0.5, self.update_time).start()
         else: self.update_interface()
