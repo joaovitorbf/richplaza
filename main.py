77547311c6402a442503ae5dfee3bf96f3c089a0
João Fonsaka <joaovitorbf@protonmail.com>
2022-05-01 01:46:04 -0300

    Add freeze support

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from ui import UserInterface
 from session import Session
 from player import Player
 from presence import Presence
+from multiprocessing import freeze_support
 
 if __name__ == "__main__":
+    freeze_support()
     presence = Presence()
     player = Player()
     ui = UserInterface(player, presence)
